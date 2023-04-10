4d17c148608f7a03d0c33f37decbf550c8cd9aae
Your Name <gustavos@vegait.com>
2023-04-10 08:57:15 -0300

    :up:

--- src/components/Background/index.jsx
+++ src/components/Background/index.jsx
@@ -8,7 +8,7 @@ export function Background({ children }) {
         <>
             <section className={style['bg-container']}>
                 <div className={style['teste-div']}>
-                    <Image src={'/bg.webp'} width={'350'} height={'250'} alt="Quiz" />
+                    <Image className={style['teste-img']} src={'/bg.webp'} width={'350'} height={'250'} alt="Quiz" />
                 </div>
                 {/* <Image className={`${style['bg-image']} ${style['zoom']}`} src={'/bg.webp'} width={1920} height={1080} loading="eager" alt="quiz - Spilinsh" /> */}
                 <div className={style['bg-image-mask']}></div>
